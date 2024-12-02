f5133d7c2575cca96f70f37ddb2e16beced6709d
Renz De Leon <renznicoledeleon19@gmail.com>
2024-12-02 23:47:06 +0800

    Add different wait

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -3,7 +3,7 @@ import exp from 'constants';
 const path = require('path')
 
 test.describe('Upload File', () => {
-    test('has title', async ({ page }) => {
+    test('Upload File', async ({ page }) => {
         //Goto URL
         await page.goto("https://practice.sdetunicorns.com/cart/");
         //Confirm correct URL
@@ -12,7 +12,7 @@ test.describe('Upload File', () => {
         //Provide path for the sample file to be uploaded
         const filePath = path.join(__dirname,'../data/logotitle.png');
 
-        //For input file find the 
+        //Upload input file  
         await page.setInputFiles('input#upfile_1', filePath);
 
         //Upload File
@@ -21,5 +21,80 @@ test.describe('Upload File', () => {
         //Assert file upload
         await expect(page.locator('#wfu_messageblock_header_1_label_1'))
         .toContainText('File logotitle.png uploaded successfully');
+    })
+})
+
+test.describe('Upload File from hidden path', () => {
+    test('Upload hidden file', async ({ page }) => {
+        //Goto URL
+        await page.goto("https://practice.sdetunicorns.com/cart/");
+        //Confirm correct URL
+        await expect(page).toHaveTitle("Cart – Practice E-Commerce Site");
+
+        //Provide path for the sample file to be uploaded
+        const filePath = path.join(__dirname,'../data/logotitle.png');
+
+        //Manipulate DOM if input file is hidded and not interactable
+        await page.evaluate(() => {
+            const selector = document.querySelector('input#upfile_1');
+            if(selector){
+                selector.className = ''
+            }
+        })
+
+        //Upload input file
+        await page.setInputFiles('input#upfile_1', filePath);
+
+        //Upload File
+        await page.locator('input#upload_1').click();
+        
+        //Assert file upload
+        await expect(page.locator('#wfu_messageblock_header_1_label_1'))
+        .toContainText('File logotitle.png uploaded successfully');
+    })
+})
+
+test.describe('Upload File Wait', () => {
+    test('Wait for state', async ({ page }) => {
+        //Goto URL
+        await page.goto("https://practice.sdetunicorns.com/cart/");
+        //Confirm correct URL
+        await expect(page).toHaveTitle("Cart – Practice E-Commerce Site");
+
+        //Provide path for the sample file to be uploaded
+        const filePath = path.join(__dirname,'../data/3mb-file.pdf');
+
+        //Upload input file  
+        await page.setInputFiles('input#upfile_1', filePath);
+
+        //Upload File
+        await page.locator('input#upload_1').click();
+        
+        await page.locator('#wfu_messageblock_header_1_label_1')
+            .waitFor({ state: 'visible', timeout: 10000})
+
+        //Assert file upload
+        await expect(page.locator('#wfu_messageblock_header_1_label_1'))
+        .toContainText('uploaded successfully');
+    })
+
+    test('Assertion Timeout', async ({ page }) => {
+        //Goto URL
+        await page.goto("https://practice.sdetunicorns.com/cart/");
+        //Confirm correct URL
+        await expect(page).toHaveTitle("Cart – Practice E-Commerce Site");
+
+        //Provide path for the sample file to be uploaded
+        const filePath = path.join(__dirname,'../data/3mb-file.pdf');
+
+        //Upload input file  
+        await page.setInputFiles('input#upfile_1', filePath);
+
+        //Upload File
+        await page.locator('input#upload_1').click();
+
+        //Assert file upload
+        await expect(page.locator('#wfu_messageblock_header_1_label_1'))
+        .toContainText('uploaded successfully', {timeout: 15000});
     })
 })
