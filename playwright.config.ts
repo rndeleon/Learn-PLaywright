95ff37c0e9305b4bda64b83756a9792f7145f937
Renz De Leon <renznicoledeleon19@gmail.com>
2025-04-06 13:26:56 +0800

    Added Allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['allure-playwright'],['line']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: require.resolve('./utils/globalSetup.ts'),
   use: {
@@ -30,7 +30,7 @@ export default defineConfig({
     baseURL: 'https://practice.sdetunicorns.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'retain-on-failure',
 
     storageState: 'loggedInState.json'
   },
