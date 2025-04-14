1221c2738bd385c6b1f84bc6d1ed25132cdb9cae
Renz De Leon <renznicoledeleon19@gmail.com>
2025-04-14 21:55:10 +0800

    Update e2e file

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['allure-playwright',{outputFolder: 'test-results'}],['line']],
-  
+  reporter: [
+    ['allure-playwright', { outputFolder: 'test-results' }],
+    ['line']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: require.resolve('./utils/globalSetup.ts'),
   use: {
